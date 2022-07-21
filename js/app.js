a362e9b06cbecb08a753ce0d656cc0b606b75774
FrancoChoclo <francorigonipersonal@gmail.com>
2022-07-20 22:34:12 -0300

    Agregue animacion, Pequeños ajustes al Color-Switch

--- js/app.js
+++ js/app.js
@@ -4,11 +4,16 @@ let section = document.getElementsByClassName("main__presentacion")
 // Ocultar cuadro de colores
 let Ajustes = document.getElementsByClassName("color-switch_Ajuste")
 let caja_ajustes = document.getElementsByClassName("color-switch")
+// Icono del Gear ajustes
+let icono = document.getElementById("Gear")
+//letras del Switch
+let h3 = document.getElementsByClassName("h3_cambiar_color")
 // Reset
 let guardar_section = window.getComputedStyle(section[0])
 let guardar_section_background = guardar_section.getPropertyValue("background")
 //let array = ["green","yellow","red","pink","gray","blue"]
 
+// Agregamos interacion a la rueda para cuando apretemos se oculte y se abra.
 let contador = 0;
 Ajustes[0].addEventListener("click", function() {
     if (contador == 0) {
@@ -20,16 +25,25 @@ Ajustes[0].addEventListener("click", function() {
     }
 })
 
+// ForEach Para que con cada iteracion de los indices de los botones querySelectorAll[indices]
+// Se valla aplicando con cada iteracion el color del atributo del html Entonces con solo cambiar,
+// El Js-color del html se cambiara los botones, el texto y la rueda de ajustes.
+
 botones.forEach ((colores, indice) => {
     console.log(colores)
     let guardar = botones[indice].attributes[0].value
     colores.style.background = guardar                 //array[indice]
+
 })
+
+// ForEach Para El evento de cuando hagas clic en el boton redondo
 
 botones.forEach( (color) => {
     color.addEventListener ("click", () => {
         let guardar = color.attributes[0].value
         section[0].style.background = guardar
+        icono.style.color = guardar
+        h3[0].style.color = guardar
         })
     })
 
